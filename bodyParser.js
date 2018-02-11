730caa998fc5e3e4f18d43c5698bc503ea4e880e
Marc Güell Segarra <marcgs@tinet.org>
2018-02-11 20:27:30 +0100

    Adjustments in bodyparser

--- bodyParser.js
+++ bodyParser.js
@@ -6,12 +6,11 @@ const config = require('./config');
 
 const BodyParser = {
     parse: (body) => {
-        // console.log(body);
-        if (body.indexOf(config.not_found_text) !== -1) {
+        if (config.not_found_text && body.indexOf(config.not_found_text) !== -1) {
             return Promise.resolve(LinkStatus.NOT_FOUND);
         }
     
-        if (body.indexOf(config.not_authenticated_text) !== -1) {
+        if (config.not_authenticated_text && body.indexOf(config.not_authenticated_text) !== -1) {
             return Promise.resolve(LinkStatus.NOT_AUTHENTICATED);
         }
     
